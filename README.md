8bbaad1dead88e8d5fc7ac0e6468a9b824926d2a
Dave Robinson <windows_mcp@hotmail.com>
2026-09-04 06:11:41 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,32 @@
+# WindowsService1
+
+VB.NET working copy of `WindowsService1` from Dave Robinson's OneDrive Historical Dev `VB/Old` folder. Project title: WindowsService1.
+
+**Source last updated:** 2026-08-27 · **Language:** VB.NET · **Target:** .NET Framework (legacy VB.NET) · **Output:** WinForms exe
+
+_Note: original OneDrive LastWriteTime values were wiped to 2026-08-27 by a zip transfer; date above uses best available evidence (headers/copyright where helpful)._
+
+## Solution structure
+
+| Project | Language | Type | Purpose |
+|---------|----------|------|---------|
+| `WindowsService1` (`WindowsService1.vbproj`) | VB.NET | WinForms exe | WindowsService1 |
+
+## How to open
+
+Open the solution in Visual Studio:
+- `WindowsService1.sln`
+
+## Requirements
+
+- Visual Studio .NET 2002 / 2003 or later that can open legacy VB.NET projects
+- Original ProductVersion: 7.0.9254
+- .NET Framework with Windows Service project support
+
+## Attribution and provenance
+
+Working copy from Dave Robinson's OneDrive Historical Dev folder `VB/Old/DOT NET/WindowsService1`.
+
+## License
+
+MIT © 2026 VaderConsulting for Dave Robinson's code. See `LICENSE`.
---
+++ Service1.vb
@@ -0,0 +1,55 @@
+Imports System.ServiceProcess
+
+Public Class Service1
+    Inherits System.ServiceProcess.ServiceBase
+
+#Region " Component Designer generated code "
+
+    Public Sub New()
+        MyBase.New()
+
+        ' This call is required by the Component Designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call
+
+    End Sub
+
+    ' The main entry point for the process
+    Shared Sub Main()
+        Dim ServicesToRun() As System.ServiceProcess.ServiceBase
+
+        ' More than one NT Service may run within the same process. To add
+        ' another service to this process, change the following line to
+        ' create a second service object. For example,
+        '
+        '   ServicesToRun = New System.ServiceProcess.ServiceBase () {New Service1, New MySecondUserService}
+        '
+        ServicesToRun = New System.ServiceProcess.ServiceBase () {New Service1}
+
+        System.ServiceProcess.ServiceBase.Run(ServicesToRun)
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.Container
+
+    ' NOTE: The following procedure is required by the Component Designer
+    ' It can be modified using the Component Designer.  
+    ' Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+        components = New System.ComponentModel.Container()
+        Me.ServiceName = "Service1"
+    End Sub
+
+#End Region
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        ' Add code here to start your service. This method should set things
+        ' in motion so your service can do its work.
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        ' Add code here to perform any tear-down necessary to stop your service.
+    End Sub
+
+End Class
